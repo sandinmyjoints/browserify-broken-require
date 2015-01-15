a49b93409505aed11e0193c224a5aced248b2ce1
Daiwei <daiweilu123@gmail.com>
2015-01-15 14:04:06 -0500

    Update bundle

--- dist/desktop.bundle.js
+++ dist/desktop.bundle.js
@@ -1,19 +1,19 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./common/boop":[function(require,module,exports){
+module.exports = function () {
+  return 'boop';
+};
+
+},{}],1:[function(require,module,exports){
 var beep = require('./desktop/beep');
 var boop = require('./common/boop');
 
 console.log('Hello ' + beep());
 
-},{"./common/boop":undefined,"./desktop/beep":2}],2:[function(require,module,exports){
+},{"./common/boop":"./common/boop","./desktop/beep":2}],2:[function(require,module,exports){
 var boop = require('../common/boop')
 
 module.exports = function () {
   return boop() + ' beep';
 };
 
-},{"../common/boop":undefined}],"/common/boop.js":[function(require,module,exports){
-module.exports = function () {
-  return 'boop';
-};
-
-},{}]},{},[1]);
+},{"../common/boop":undefined}]},{},[1]);
